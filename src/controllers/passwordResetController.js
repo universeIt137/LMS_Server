99a54fe5da85578086f0a47d89acb8615b09b179
ishanrana363 <ishanrana363@gmail.com>
2024-07-31 22:37:39 +0600

    email send api created

---
+++ src/controllers/passwordResetController.js
@@ -0,0 +1,51 @@
+const otpModel = require("../models/otpModel");
+const userModel = require("../models/userModel");
+const sendEmailUtility = require("../utility/sendEmailUtility");
+const jwt = require("jsonwebtoken");
+class passwordResetClass {
+    sendEmailUser = async (req,res) => {
+        try {
+            let email = req.params.email;
+            const otp = Math.floor(Math.random() * 1000000);
+            const emailText = `Your verification code is ${otp}`;
+            const emailSubject = `LMS Verification code `;
+            const status = 1;
+            let userData = await userModel.findOne({email:email});
+            const jwtToken = jwt.sign({
+                email: email,
+                exp: Math.floor(Date.now() / 1000) + (30), // 30 sec
+            },process.env.OTP_SECRET);
+
+
+            if (userData){
+                await sendEmailUtility(email,emailText,emailSubject);
+                await otpModel.create({
+                    otp : otp,
+                    token:jwtToken,
+                    status:status
+                })
+                return res.status(200).json({
+                    status:"success",
+                    data : "6 digit otp code send successfully"
+                });
+            }else {
+                return res.status(404).json({
+                    status:"fail",
+                    msg:"User not found"
+                });
+            }
+        }catch (e) {
+            console.log(e);
+            return res.status(500).json({
+                status:"fail",
+                msg:"something went worng"
+            });
+        }
+    };
+
+}
+
+
+const passwordResetController = new passwordResetClass();
+
+module.exports = passwordResetController;
