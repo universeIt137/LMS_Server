780579a17fc215ca687d19bd61cdd9d67563e3d4
ishanrana363 <ishanrana363@gmail.com>
2024-08-01 10:38:22 +0600

    instructor create api completed

--- src/routes/privateRoute.js
+++ src/routes/privateRoute.js
@@ -2,6 +2,7 @@ const express = require("express");
 const router = express.Router();
 const userController = require("../controllers/userControllers");
 const authMiddleware = require("../middleware/authMiddleware");
+const instructorController = require("../controllers/instructorController");
 
 
 
@@ -9,6 +10,13 @@ router.put("/update-user/:id", authMiddleware.isValidUser, userController.update
 router.delete("/delete-user/:id", authMiddleware.isValidUser, userController.deleteUser);
 router.get("/single-user", authMiddleware.isValidUser, userController.singleUser);
 router.get("/all-user", authMiddleware.isValidUser, userController.allUser);
+
+
+// instructor api
+
+router.post("/instructor-create", authMiddleware.isSuperAdmin, instructorController.instructorCreate);
+
+
 
 
 module.exports = router
