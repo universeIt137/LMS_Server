a7147ecd1aaf1d8c4972980fd2f17b1bdeb64673
ishanrana363 <ishanrana363@gmail.com>
2024-08-22 15:43:51 +0600

    mongodb database update

--- src/controllers/user/userController.js
+++ src/controllers/user/userController.js
@@ -1,20 +1,18 @@
 const userModel = require("../../models/userModel");
 
 class userClass {
-    signUp =  (req,res)=>{
+    signUp = async (req,res)=>{
         try {
-            let reqBody = req.body;
-            console.log(reqBody);
-            let data = userModel.create(reqBody);
-            return res.status(201).json({
-                status : 'success',
-                data : data
-            })
-        } catch (e) {
-            return res.status(500).json({
-                status : "fail",
-                msg : e.toString()
-            });
+        let reqBody = req.body;
+        console.log(reqBody);
+        let data = await userModel.create(reqBody);
+        return res.status(201).send({
+            status:"success",
+            data : data
+        })
+        } catch (error) {
+            console.log(error);
+            
         }
     };
 
