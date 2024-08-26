a22ad5ff0cd599d6f32034a987b1b15d07e08c7a
ishanrana363 <ishanrana363@gmail.com>
2024-08-26 11:25:15 +0600

    log

--- src/middlewares/authMiddleware.js
+++ src/middlewares/authMiddleware.js
@@ -4,7 +4,7 @@ const accessTokenKey = process.env.ACCESS_TOKEN_KEY;
 const isLogIn = (req, res, next) => {
     try {
         const token = req.cookies.accessToken;
-        
+        console.log(`vercel token is ${token}`)
         if (!token) {
             return res.status(401).json({
                 status: "fail",
