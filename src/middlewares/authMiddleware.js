1c1759862601f8c1bb658d25ab731428be14e7e0
ishanrana363 <ishanrana363@gmail.com>
2024-08-26 10:48:11 +0600

    some change

--- src/middlewares/authMiddleware.js
+++ src/middlewares/authMiddleware.js
@@ -5,7 +5,7 @@ const isLogIn = (req, res, next) => {
     try {
         const token = req.cookies.accessToken;
         console.log(token);
-
+        
         if (!token) {
             return res.status(401).json({
                 status: "fail",
