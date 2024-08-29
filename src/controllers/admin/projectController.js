e60694336057f1d557f5cf67313b4a995bd79751
ishanrana363 <ishanrana363@gmail.com>
2024-08-29 14:23:39 +0600

    get all project by admin api created

--- src/controllers/admin/projectController.js
+++ src/controllers/admin/projectController.js
@@ -43,6 +43,7 @@ class projectClass {
             });
         }
     };
+
     deleteProject = async (req,res)=>{
         try {
             let id = req.params.id;
@@ -66,6 +67,26 @@ class projectClass {
             });
         }
     }
+
+    getAllProjectByAdmin = async (req,res)=>{
+        try {
+            let data = await projectModel.find();
+            if(data.length===0) return res.status(404).json({
+                status:"fail",
+                msg : "Project data not found"
+            });
+            return res.status(200).json({
+                status:"success",
+                msg : "find all project data successfully",
+                data : data
+            })
+        } catch (error) {
+            return res.status(500).json({
+                status:"fail",
+                msg : error.toString()
+            });
+        }
+    };
 
 }
 
