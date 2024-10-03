0c18e6f9dddfb1ed18e6814ac869cf0d51f3bc55
ishanrana363 <ishanrana363@gmail.com>
2024-10-03 17:16:03 +0600

    all get in course api modify

--- src/controllers/admin/getInCourseController.js
+++ src/controllers/admin/getInCourseController.js
@@ -1,4 +1,5 @@
 const getInCourseModel = require("../../models/getInCourseModel");
+const getInCourseService = require("../../services/getInService");
 
 class getInCourseClass {
     create = async (req,res)=>{
@@ -67,23 +68,8 @@ class getInCourseClass {
     };
 
     allGetInCourseAdmin = async (req,res)=>{
-        try {
-            let data = await getInCourseModel.find();
-            if(data.length===0) return res.status(404).json({
-                status : "fail",
-                msg : "getInCourse data not found"
-            });
-            return res.status(200).json({
-                status:"success",
-                msg : "all data find successfully ",
-                data : data
-            })
-        } catch (error) {
-            return res.status(500).json({
-                status:"fail",
-                msg : error.toString()
-            });
-        }
+        let data = await getInCourseService.allGetInService();
+        res.send(data);
     };
 
 }
