2edc3de0621c091ef0f5f5cac48d0bac81cde6bb
ishanrana363 <ishanrana363@gmail.com>
2024-10-16 13:29:02 +0600

    poroject by course id api created

--- src/services/instructorService.js
+++ src/services/instructorService.js
@@ -28,7 +28,6 @@ class instructorClass {
             let data = await instructorModel.aggregate([
                 joinWithCourseId,
                 unwindCourseData,
-                projection
             ]);
 
             if(data.length < 0) {
@@ -82,7 +81,6 @@ class instructorClass {
                 matchStage,
                 joinWithCourseId,
                 unwindCourseData,
-                projection
             ]);
 
             if(data.length < 0) {
@@ -105,6 +103,59 @@ class instructorClass {
             }
         }
     };
+
+    instructorByCourseId = async (req)=>{
+        let id = new mongoose.Types.ObjectId(req.params.courseId);
+        try {
+            // match stage
+            let matchStage = { $match: { course_id: id } };
+            // join with course id 
+            let joinWithCourseId = {
+                $lookup: {
+                    from: "courses",
+                    localField: "course_id",
+                    foreignField: "_id",
+                    as: "courseData"
+                }
+            };
+            // unwind courseData
+            const unwindCourseData = {  $unwind: "$courseData" };
+            // projection courseData
+            const projection = { $project : {
+                "instructor_name" : 1,
+                "instructor_img" : 1,
+                "instructor_role" : 1,
+                "courseData.course_name" :  1,
+            }};
+            let data = await instructorModel.aggregate([
+                matchStage,
+                joinWithCourseId,
+                unwindCourseData,
+            ]);
+            if(data.length < 0) {
+                return {
+                    status: "fail",
+                    message: "No instructor found for this course",
+                };
+            }
+            return {
+                status: "success",
+                message: "Instructors retrieved successfully for the course",
+                data : data 
+            }; 
+            
+        } catch (error) {
+            return {
+                status : "fail",
+                message : error.toString()
+            }
+        }
+        
+
+    };
+
+
+
 }
 
 const instructorService = new instructorClass();
