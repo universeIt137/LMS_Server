63195bccbc5413799866ae219ca8dc3d527b7c78
ishanrana363 <ishanrana363@gmail.com>
2024-10-20 11:47:41 +0600

    single course details api created

--- src/services/courseDetailsService.js
+++ src/services/courseDetailsService.js
@@ -45,6 +45,43 @@ class courseDetailsClass {
             }
         }
     };
+    singleCourseDetailsService = async (req)=>{
+        let id = new mongoose.Types.ObjectId(req.params.id);
+        try {
+            let matchStage = {$match : { _id : id }};
+            let joinWithCourseId = {
+                $lookup: {
+                    from: "courses",
+                    localField: "course_id",
+                    foreignField: "_id",
+                    as: "courseData"
+                }
+            };
+            let unwindCourseData = { $unwind: "$courseData" };
+            let data = await courseDetailsModel.aggregate([
+                matchStage,
+                joinWithCourseId,
+                unwindCourseData
+            ]);
+            if(data.length===0){
+                return {
+                    status: "fail",
+                    msg: "Data not found",
+                };
+            }
+            return {
+                status: "success",
+                msg: "Get single course details successfully",
+                data: data,
+            };
+            
+        } catch (error) {
+            return {
+                status: "fail",
+                msg: error.toString(),
+            }
+        }
+    }
 }
 
 const courseDetailsService = new courseDetailsClass();
